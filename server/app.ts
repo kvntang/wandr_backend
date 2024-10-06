892741c25ccd1088c72949bd0888b211f1e6cc7f
Kevin <kevintang1509@gmail.com>
2024-10-06 17:59:13 -0400

    initiated autoCaptioning concept

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,9 @@
 import AuthenticatingConcept from "./concepts/authenticating";
+import AutoCaptioningConcept from "./concepts/autocaptioning";
 import CommentingConcept from "./concepts/commenting";
 import FriendingConcept from "./concepts/friending";
 import PostingConcept from "./concepts/posting";
 import SessioningConcept from "./concepts/sessioning";
-
-// import SuggestingConcept from "./concepts/suggesting";
-// import SummarizingConcept from "./concepts/summarizing";
-// import CategorizingConcept from "./concepts/categorizing";
 
 // The app is a composition of concepts instantiated here
 // and synchronized together in `routes.ts`.
@@ -15,7 +12,4 @@ export const Authing = new AuthenticatingConcept("users");
 export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
 export const Commenting = new CommentingConcept("comments");
-
-// export const Suggesting = new SuggestingConcept();
-// export const Summarizing = new SummarizingConcept();
-// export const Categorizing = new CategorizingConcept();
+export const AutoCaptioning = new AutoCaptioningConcept("auto-captions");
